8eec269eb78c1b52bdcc223d2566568370bf2167
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-10-19 14:08:28 +0000

    Front-end Development

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
+
 function animation() {
     const image=document.querySelector("#img")
   
